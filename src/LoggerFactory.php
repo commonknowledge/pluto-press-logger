25895afdcdef09daf808f468d0756274370f05ca
Joaquim d'Souza <joaquimds@gmail.com>
2023-11-22 18:42:48 +0100

    feat: add LogglyHandler to Logger

--- src/LoggerFactory.php
+++ src/LoggerFactory.php
@@ -2,6 +2,8 @@
 
 namespace CommonKnowledge\Monolog;
 
+use Env\Env;
+use Monolog\Handler\LogglyHandler;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger;
 
@@ -18,6 +20,13 @@ class LoggerFactory
             $log_dir = dirname(__DIR__, 4) . '/logs';
         }
         $logger->pushHandler(new RotatingFileHandler($log_dir . '/' . $name . '.log', maxFiles: 32));
+
+        $loggly_api_key = Env::get("LOGGLY_CUSTOMER_TOKEN");
+
+        if ($loggly_api_key) {
+            $logger->pushHandler(new LogglyHandler("{$loggly_api_key}/tag/monolog'"));
+        }
+
         return $logger;
     }
 }
